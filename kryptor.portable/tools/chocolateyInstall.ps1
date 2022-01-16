1c4bcce79121b64e8ef211a292ab5e2dcbe9212a
samuel-lucas6 <63159663+samuel-lucas6@users.noreply.github.com>
2022-01-16 11:57:01 +0000

    kryptor.portable: Upload files.

---
+++ kryptor.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+    packageName    = $Env:ChocolateyPackageName
+    unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+    url64          = 'https://github.com/samuel-lucas6/Kryptor/releases/download/v3.1.0/kryptor-windows.zip'
+    checksum64     = 'C6142E7B987C390EF9BC097273EC9E4284D901619E3F74C97A6F4E15430B7DA6'
+    checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
