d8227da5d393b05e624dd29d4588052f888ed53d
samuel-lucas6 <63159663+samuel-lucas6@users.noreply.github.com>
2022-09-20 09:30:24 +0100

    kryptor: v4.0.0.

--- kryptor/tools/chocolateyInstall.ps1
+++ kryptor/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
     packageName    = $Env:ChocolateyPackageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-    url64          = 'https://github.com/samuel-lucas6/Kryptor/releases/download/v3.1.1/kryptor-windows.zip'
-    checksum64     = 'E5278429C3C52CCF7FB5B8FA35289D505A3266C68CD7D65796B6699111F63815'
+    url64          = 'https://github.com/samuel-lucas6/Kryptor/releases/download/v4.0.0/kryptor-windows-x64.zip'
+    checksum64     = '5B8FF5581FFFCD695EEA055E36B97D838C218CCAFDB6980A8154D1164E28D7B8'
     checksumType64 = 'sha256'
 }
 
